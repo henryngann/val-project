7e7ca65fdc0303c1ed5ae14c4e62a54d1bb1735a
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2022-09-05 19:42:50 -0400

    format

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,34 @@
+import { Grid, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const LandingPage = () => {
-  return <section>LandingPage</section>;
+  const sectionStyles = {
+    width: "100%",
+    height: "100vh",
+  };
+
+  return (
+    <Grid
+      container
+      display="flex"
+      component={"section"}
+      sx={sectionStyles}
+      direction={"column"}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid item xs={3}>
+        <Typography>Val Logo</Typography>
+      </Grid>
+      <Grid item xs={3}>
+        <Typography>Tabs</Typography>
+      </Grid>
+      <Grid item xs={5}>
+        Content
+      </Grid>
+      <Grid item>Footer</Grid>
+    </Grid>
+  );
 };
 
 export default LandingPage;
