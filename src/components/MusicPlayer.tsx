2062c22a499ce0827fab437ed34248aab77c38b7
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2022-11-05 17:29:23 -0400

    add music Player

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeMuteIcon from "@mui/icons-material/VolumeMute";
+import { Button } from "@mui/material";
 
 const MusicPlayer = () => {
-  return <div>MusicPlayer</div>;
+  //path to bgmusic.mp3
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const musicSrc = "../assets/bgmusic.mp3";
+  const audio = new Audio(musicSrc);
+
+  const handlePlay = () => {
+    setIsPlaying(!isPlaying);
+    if (isPlaying) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  };
+
+  return <Button>{isPlaying ? <VolumeUpIcon /> : <VolumeMuteIcon />} </Button>;
 };
 
 export default MusicPlayer;
