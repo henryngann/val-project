c0483aa5b98211adbdccc6d4cced642e372b342e
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2022-11-05 16:36:37 -0400

    add music player file, checkpoint

---
+++ src/components/MusicPlayer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import VolumeMuteIcon from "@mui/icons-material/VolumeMute";
+
+const MusicPlayer = () => {
+  return <div>MusicPlayer</div>;
+};
+
+export default MusicPlayer;
