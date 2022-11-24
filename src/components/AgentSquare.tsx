97533d31e8f1b062e788c0f6fb88c0d31e6596e4
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2022-11-24 16:32:31 -0500

    checkpoint

--- src/components/AgentSquare.tsx
+++ src/components/AgentSquare.tsx
@@ -1,12 +1,7 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Box } from "@mui/system";
-
+import { motion } from "framer-motion/dist/framer-motion";
 interface AgentPolaroid {
   agentImage: string;
   agentName: string;
@@ -15,17 +10,15 @@ interface AgentPolaroid {
 export default function AgentSquare({ agentImage, agentName }: AgentPolaroid) {
   return (
     <>
-      <Box component="figure">
-        <img
-          src={agentImage}
-          alt={agentName}
-          width={80}
-          height={80}
-          style={{ border: "1px solid grey", borderRadius: "2px" }}
-        />
-      </Box>
-
-      <Typography variant="subtitle2">{agentName}</Typography>
+      <motion.img
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        src={agentImage}
+        alt={agentName}
+        width={80}
+        height={80}
+        style={{ border: "1px solid grey", borderRadius: "2px" }}
+      />
     </>
   );
 }
