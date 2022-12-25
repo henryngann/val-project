21fcd4428d470201232cd81dd099d4583ade3217
Henry Ngan <64046050+henryngann@users.noreply.github.com>
2022-12-25 18:52:46 -0500

    set up hook

--- src/hooks/useFetchUuid.tsx
+++ src/hooks/useFetchUuid.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const useFetchUuid = () => {
+  // const [result, setResult] = useState<Root>();
+  // const [error, setError] = useState();
+  // const [isLoading, setIsLoading] = useState(true);
+
   return <div>useFetchUuid</div>;
 };
 
